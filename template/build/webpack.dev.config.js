5cb04046888b3d07f6e0510bab8babe00d47769e
chenzhigang <979703986@qq.com>
2023-03-06 17:41:32 +0800

    chore: 测试

--- template/build/webpack.dev.config.js
+++ template/build/webpack.dev.config.js
@@ -64,4 +64,36 @@ module.exports = merge(webpackCommonConfig, {
       ],
     }),
   ],
+
+  // 拆分 分包
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      minSize: 1 * 1, // 分割 chunk 最小为 kb
+      maxSize: 0, // 最大没有限制
+      minChunks: 1, // 要提取的 chunk 最少被引用一次
+      maxAsyncRequests: 5, // 按需加载时并行加载的文件最大数
+      maxInitialRequests: 3, // 入口 js 文件最大并行数量
+      automaticNameDelimiter: '~', // 名称连接符
+      name: true,
+      cacheGroups: {
+        // 分割 chunk 的组
+        // node_modules 文件会被打包到 vendors 组的 chunk 中 `vendors~xxx.js`
+        // 需要满足上面的规则
+        vendors: {
+          name: 'vendors',
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10, // 优先级
+        },
+        commons: {
+          // 要提取的 chunk 最少被引用 2次
+          name: 'commons',
+          minChunks: 2,
+          priority: -10, // 优先级
+          // 如果当前要打包的模块，和之前已经被提取的模块是同一个，就会被复用
+          reuseExistingChunk: true,
+        },
+      },
+    },
+  },
 })
--- template/src/views/index/index.js
+++ template/src/views/index/index.js
@@ -1,3 +1,10 @@
 import './index.scss'
 import 'src/components/header'
 import './components/footer'
+import { omit, pick } from '@daysnap/utils'
+
+const data = { a: 1, b: 2, c: 3 }
+
+const res = pick(data, ['a'])
+const res2 = omit(data, ['a'])
+console.log('res => ', res, res2)
