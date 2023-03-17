647c7e6cf4a18991ebde8558107bc411ed174a8b
wanghaibo <1056140533@qq.com>
2023-03-17 16:01:38 +0800

    feat: git钩子验证。

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,16 +1,21 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-    }
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  plugins: ['prettier'],
+  extends: [
+    'eslint:recommended',
+    'eslint-config-prettier',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+  },
 }
--- template/src/views/home/index.js
+++ template/src/views/home/index.js
@@ -1,5 +1,6 @@
 import 'src/components/header/index.js'
 let array = ['1', '2', '3']
+var a = 123
 
 if (array.includes('2')) {
   console.log('有')
